e1ea09991dfa5aae0c390e0736b04e9fb87b99b2
Myo Thet <mr.myothet@gmail.com>
2024-04-13 16:07:50 +0700

    Ado.net Read - SqlCommand, SqlDataAdapter, DataTable, DataRow, DataColumn

--- MTDotNetCore.ConsoleApp/Program.cs
+++ MTDotNetCore.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
@@ -13,12 +14,34 @@ stringBuilder.InitialCatalog = "DotNetTrainingBatch4"; // database name
 stringBuilder.UserID = "sa";
 stringBuilder.Password = "admin123!";
 
-// SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-SqlConnection connection = new SqlConnection("Data Source=SOLIDCAD-SERVER\\SQLEXPRESS2012;Initial Catalog=DotNetTrainingBatch4;User ID=sa;Password=admin123!");
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+// SqlConnection connection = new SqlConnection("Data Source=SOLIDCAD-SERVER\\SQLEXPRESS2012;Initial Catalog=DotNetTrainingBatch4;User ID=sa;Password=admin123!");
 connection.Open();
 Console.WriteLine("Connection Open.");
 
+// Ado.Net Read
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
 connection.Close();
 Console.WriteLine("Connection Close.");
+
+/* 
+ * DataSet => DataTable
+ * DataTable => DataRow
+ * DataRow => DataColumn
+*/
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+    Console.WriteLine("Blog Id => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Id => " + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Id => " + dr["BlogContent"]);
+    Console.WriteLine("--------------------------------");
+}
 
 Console.ReadKey();
