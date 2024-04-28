8d8189f01aca58af2b68f3897363a748e4bce563
Myo Thet <mr.myothet@gmail.com>
2024-04-28 21:57:02 +0700

    DapperExample - Create

--- MTDotNetCore.ConsoleApp/DapperExample.cs
+++ MTDotNetCore.ConsoleApp/DapperExample.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +14,11 @@ namespace MTDotNetCore.ConsoleApp
     {
         public void Run()
         {
-            Read();
-            Edit(1);
-            Edit(12);
+            //Read();
+            //Edit(1);
+            //Edit(12);
+
+            Create("title", "author", "content");
 
         }
 
@@ -38,7 +41,7 @@ namespace MTDotNetCore.ConsoleApp
         private void Edit(int id)
         {
             using IDbConnection db = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
-            
+
             var item = db.Query<BlogDto>("select * from tbl_blog where blogid = @BlogId", new BlogDto { BlogId = id }).FirstOrDefault();
 
             if (item is null)
@@ -53,5 +56,33 @@ namespace MTDotNetCore.ConsoleApp
             Console.WriteLine(item.BlogContent);
             Console.WriteLine("------------------------------");
         }
+
+        private void Create(string title, string author, string content)
+        {
+            var item = new BlogDto 
+            { 
+                BlogTitle = title, 
+                BlogAuthor = author,
+                BlogContent = content,
+            };
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                           ([BlogTitle]
+                           ,[BlogAuthor]
+                           ,[BlogContent])
+                     VALUES
+                           (@BlogTitle
+                           ,@BlogAuthor
+                           ,@BlogContent)";
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Saving successful." : "Saving failed.";
+            Console.WriteLine(message);
+
+        }
+
     }
 }
