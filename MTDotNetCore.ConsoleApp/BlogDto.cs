fc138c9c6302ee502926ebcaa5454759bf360c2c
Myo Thet <mr.myothet@gmail.com>
2024-04-28 15:55:25 +0700

    Dapper - Add BlogDto class

---
+++ MTDotNetCore.ConsoleApp/BlogDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDotNetCore.ConsoleApp;
+
+public class BlogDto
+{
+    public int BlogId { get; set; }
+
+    public string BlogTitle { get; set; }
+
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+
+}
+
+//public record BlogIdentity(int Blogid, string BlogTitle, string BlogAuthor, string BlogContent);
--- README.md
+++ README.md
@@ -3,3 +3,7 @@ UI + Business Logic + Data Access + Db
 Mobile, Web => API => Database
 
 C# => Db
+
+2024-04-09--ConsoleApp
+2024-04-10--Ado.Net
+2024-04-22--Dapper
