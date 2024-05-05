d15037735e02d65c24119a0352da0b92014c7854
Myo Thet <mr.myothet@gmail.com>
2024-05-06 00:29:10 +0700

    RestApi - Dapper - Controller initialization for HTTP methods

---
+++ MTDotNetCore.RestApi/Controllers/BlogDapperController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.RestApi.Models;
+
+namespace MTDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogDapperController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlogs(BlogModel blog)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, BlogModel blog)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, BlogModel blog)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            return Ok();
+        }
+    }
+}
