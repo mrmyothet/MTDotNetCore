7abaf6d1eb31d381fd9f83e7dbdafcc5d7da70e2
Myo Thet <mr.myothet@gmail.com>
2024-05-05 15:05:03 +0700

    RestApi - Copy ConnectionStrings, AppDbContext and BlogModel from ConsoleApp

---
+++ MTDotNetCore.RestApi/Db/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTDotNetCore.RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDotNetCore.RestApi.Db
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
