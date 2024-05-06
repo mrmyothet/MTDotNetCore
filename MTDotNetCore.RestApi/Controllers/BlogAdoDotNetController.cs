9f6427e93694f269c3fc38866eaa6bbba979d802
Myo Thet <mr.myothet@gmail.com>
2024-05-06 18:13:34 +0700

    RestApi - AdoDotNet - Read, Edit, Create

---
+++ MTDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.RestApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MTDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoDotNetController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            string query = "select * from tbl_blog";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            // Ado.Net Read
+            
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            //List<BlogModel> lst = new List<BlogModel>();
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    BlogModel blog = new BlogModel();
+
+            //    blog.BlogId =Convert.ToInt32(dr["BlogId"]);
+            //    blog.BlogTitle = Convert.ToString(dr["BlogTitle"]);
+            //    blog.BlogAuthor = Convert.ToString(dr["BlogAuthor"]);
+            //    blog.BlogContent = Convert.ToString(dr["BlogContent"]);
+
+            //    lst.Add(blog);
+            //}
+
+            List<BlogModel> lst = dt.AsEnumerable().Select(dr => new BlogModel
+            {
+                BlogId = Convert.ToInt32(dr["BlogId"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]), 
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]), 
+                BlogContent = Convert.ToString(dr["BlogContent"])
+
+            }).ToList();
+
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            string query = "select * from tbl_blog where BlogId = @BlogId";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                
+                return NotFound("No data found for the Id: " + id);
+            }
+
+            DataRow dr = dt.Rows[0];
+
+            BlogModel blog = new BlogModel
+            {
+                BlogId = Convert.ToInt32(dr["BlogId"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]),
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+                BlogContent = Convert.ToString(dr["BlogContent"])
+            };
+
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", blog.BlogTitle);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blog.BlogAuthor);
+            cmd.Parameters.AddWithValue("@BlogContent", blog.BlogContent);
+
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Saving successful." : "Saving failed.";
+
+            if (message == "Saving failed.")
+                return StatusCode(500, message);
+
+            return Ok(message);
+        }
+    }
+}
