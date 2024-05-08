d163f379d44137bebbe6927f794ffa1c746d5fd1
Myo Thet <mr.myothet@gmail.com>
2024-05-08 22:00:33 +0700

    Add ConnectionStrings class into RestApiWithNLayer project

---
+++ MTDotNetCore.RestApiWithNLayer/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDotNetCore.RestApiWithNLayer
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "MYOTHETPC\\MSSQLSERVER2012",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "admin123!",
+            TrustServerCertificate = true
+        };
+
+    }
+}
