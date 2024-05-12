0cfe02da18afab5582d108d1ce00758c9923a814
Myo Thet <mr.myothet@gmail.com>
2024-05-12 11:46:47 +0700

    Feature Blog - Controller - CRUD by using BL_Blog

--- MTDotNetCore.RestApiWithNLayer/Features/Blog/BlogController.cs
+++ MTDotNetCore.RestApiWithNLayer/Features/Blog/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MTDotNetCore.RestApiWithNLayer.Features.Blog
 {
@@ -7,5 +8,70 @@ namespace MTDotNetCore.RestApiWithNLayer.Features.Blog
     [ApiController]
     public class BlogController : ControllerBase
     {
+        private readonly BL_Blog _bL_Blog;
+
+        public BlogController()
+        {
+            _bL_Blog = new BL_Blog();
+        }
+
+
+        [HttpGet]
+        public IActionResult Read()
+        {
+            var lst = _bL_Blog.GetBlogs();
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No data found with the Id: " + id);
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogModel blog)
+        {
+            var result = _bL_Blog.CreateBlog(blog);
+
+            string message = result > 0 ? "Saving Successful" : "Saving Failed";
+            return Ok(message);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BlogModel blog)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No data found for the Id: " + id);
+            }
+
+            var result = _bL_Blog.UpdateBlog(id, blog);
+            string message = result > 0 ? "Updating Successful" : "Updating Failed";
+
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No data found with the Id: " + id);
+            }
+
+            var result = _bL_Blog.DeleteBlog(id);
+            string message = result > 0 ? "Delete Successful" : "Delete Failed";
+
+            return Ok(message);
+        }
     }
 }
