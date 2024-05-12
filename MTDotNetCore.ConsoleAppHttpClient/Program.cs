7ad6a14a03766842112e924ceef3e2c185222b80
Myo Thet <mr.myothet@gmail.com>
2024-05-12 13:43:11 +0700

    HttpClient - Create Dto by using Paste JSON as Classes

--- MTDotNetCore.ConsoleAppHttpClient/Program.cs
+++ MTDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -4,3 +4,26 @@ string jsonStr = await File.ReadAllTextAsync("data.json");
 Console.WriteLine(jsonStr);
 
 Console.ReadLine();
+
+
+// Copy the contents of json file
+// Edit => Paste Special => Paste JSON as Classes
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
