5dff0c2ade58cfadc4202522211f7e80b325ce01
Myo Thet <mr.myothet@gmail.com>
2024-05-14 11:22:10 +0700

    HttpClient - JsonConvert.SerializeObject and DeserializeObject Methods

--- MTDotNetCore.ConsoleAppHttpClientExamples/Program.cs
+++ MTDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
 
 HttpClient client = new HttpClient();
 var response = await client.GetAsync("https://localhost:7233/api/blog");
@@ -7,6 +11,25 @@ if (response.IsSuccessStatusCode)
 {
     string jsonStr = await response.Content.ReadAsStringAsync();
     Console.WriteLine(jsonStr);
+
+    List<BlogObject> lst = JsonConvert.DeserializeObject<List<BlogObject>>(jsonStr)!;
+    foreach (var item in lst)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(item));
+
+        Console.WriteLine($"Title => {item.BlogTitle}");
+        Console.WriteLine($"Author => {item.BlogAuthor}");
+        Console.WriteLine($"Content => {item.BlogContent}");
+    }
 }
 
 Console.ReadLine();
+
+
+public class BlogObject
+{
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
