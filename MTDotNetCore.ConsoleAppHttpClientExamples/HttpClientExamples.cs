df035b551e07201049c42b22527a607494421fb6
Myo Thet <mr.myothet@gmail.com>
2024-05-14 11:55:17 +0700

    HttpClientExamples - Read

---
+++ MTDotNetCore.ConsoleAppHttpClientExamples/HttpClientExamples.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDotNetCore.ConsoleAppHttpClientExamples
+{
+    internal class HttpClientExamples
+    {
+        private readonly HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("https://localhost:7233") };
+        private readonly string _blogEndpoint = "/api/blog";
+
+        public async void RunAsync()
+        {
+            //ReadAsync().Wait();
+            await ReadAsync();
+        }
+
+        private async Task ReadAsync() 
+        {
+            var response = await _httpClient.GetAsync(_blogEndpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+
+                List<BlogObject> lst = JsonConvert.DeserializeObject<List<BlogObject>>(jsonStr)!;
+                foreach (var item in lst)
+                {
+                    //Console.WriteLine(JsonConvert.SerializeObject(item));
+
+                    Console.WriteLine($"Title => {item.BlogTitle}");
+                    Console.WriteLine($"Author => {item.BlogAuthor}");
+                    Console.WriteLine($"Content => {item.BlogContent}");
+                    await Console.Out.WriteLineAsync("==========");
+                }
+            }
+        }
+    }
+}
--- MTDotNetCore.ConsoleAppHttpClientExamples/Program.cs
+++ MTDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -1,35 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using MTDotNetCore.ConsoleAppHttpClientExamples;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 Console.WriteLine("Hello, World!");
 
-HttpClient client = new HttpClient();
-var response = await client.GetAsync("https://localhost:7233/api/blog");
-
-if (response.IsSuccessStatusCode)
-{
-    string jsonStr = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(jsonStr);
-
-    List<BlogObject> lst = JsonConvert.DeserializeObject<List<BlogObject>>(jsonStr)!;
-    foreach (var item in lst)
-    {
-        Console.WriteLine(JsonConvert.SerializeObject(item));
-
-        Console.WriteLine($"Title => {item.BlogTitle}");
-        Console.WriteLine($"Author => {item.BlogAuthor}");
-        Console.WriteLine($"Content => {item.BlogContent}");
-    }
-}
+HttpClientExamples httpClientExample = new HttpClientExamples();
+httpClientExample.RunAsync();
 
 Console.ReadLine();
-
-
-public class BlogObject
-{
-    public int BlogId { get; set; }
-    public string? BlogTitle { get; set; }
-    public string? BlogAuthor { get; set; }
-    public string? BlogContent { get; set; }
-}
