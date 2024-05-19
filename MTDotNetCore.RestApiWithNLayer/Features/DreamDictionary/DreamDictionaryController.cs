e2af90d0a43e54214a396cbed25e6a2bcb70fd43
Myo Thet <mr.myothet@gmail.com>
2024-05-19 17:33:50 +0700

    BPI Homework - Dream Dictionary - Add additional Endpoints

--- MTDotNetCore.RestApiWithNLayer/Features/DreamDictionary/DreamDictionaryController.cs
+++ MTDotNetCore.RestApiWithNLayer/Features/DreamDictionary/DreamDictionaryController.cs
@@ -15,6 +15,13 @@ public class DreamDictionaryController : ControllerBase
         return model;
     }
 
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var model = await GetDataAsync();
+        return Ok(model);
+    }
+
     [HttpGet("header")]
     public async Task<IActionResult> GetDreamHeader()
     {
@@ -22,12 +29,38 @@ public class DreamDictionaryController : ControllerBase
         return Ok(model.BlogHeader);
     }
 
+    [HttpGet("header/{headerId}")]
+    public async Task<IActionResult> GetDreamHeader(int headerId)
+    {
+        var model = await GetDataAsync();
+        var dreamHeader = model.BlogHeader.FirstOrDefault(x => x.BlogId == headerId);
+
+        if (dreamHeader is null) return NotFound($"No data found for Id: {headerId}");
+
+        return Ok(dreamHeader);
+    }
+
+    [HttpGet("details/{headerId}")]
+    public async Task<IActionResult> GetDreamDetails(int headerId)
+    {
+        var model = await GetDataAsync();
+        var lst = model.BlogDetail.Where(x => x.BlogId == headerId);
+        if (lst.Count() == 0) return NotFound($"No data found for headerId {headerId}");
+
+        return Ok(lst);
+    }
+
     [HttpGet("details/{headerId}/{detailId}")]
     public async Task<IActionResult> GetDreamDetails(int headerId, int detailId)
     {
         var model = await GetDataAsync();
 
-        var dreamDetails = model.BlogDetail.FirstOrDefault(x=> x.BlogId == headerId && x.BlogDetailId == detailId);
+        var dreamHeader = model.BlogHeader.FirstOrDefault(x => x.BlogId == headerId);
+        if (dreamHeader is null) return NotFound($"No data found for headerId: {headerId}");
+
+        var dreamDetails = model.BlogDetail.FirstOrDefault(x => x.BlogId == headerId && x.BlogDetailId == detailId);
+        if (dreamDetails is null) return NotFound($"No data found for detailsId {detailId}");
+
         return Ok(dreamDetails);
     }
 }
