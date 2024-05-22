caef6a8fa0031823ba79b1b15b865af4c4d4f5e8
Myo Thet <mr.myothet@gmail.com>
2024-05-22 19:01:01 +0700

    Pizza Api - Get Pizzas

---
+++ MTDotNetCore.PizzaApi/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MTDotNetCore.PizzaApi;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<PizzaModel> Pizzas { get; set; }
+}
+
+[Table("Tbl_Pizza")]
+public class PizzaModel
+{
+    [Key]
+    [Column("PizzaId")]
+    public int Id { get; set; }
+
+    [Column("Pizza")]
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+}
---
+++ MTDotNetCore.PizzaApi/Features/Pizza/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTDotNetCore.PizzaApi.Features.Pizza
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PizzaController()
+        {
+            _dbContext = new AppDbContext();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var lst = await _dbContext.Pizzas.ToListAsync();
+            return Ok(lst);
+        }
+    }
+}
