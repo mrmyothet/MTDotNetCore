f65879c32afcd78dd2313f7d46fcdba63c67ee77
Myo Thet <mr.myothet@gmail.com>
2024-06-10 18:13:11 +0700

    Windows Form - Sql Injection - Add porject

---
+++ MTDotNetCore.WinFormsAppSqlnjection/FrmLogin.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace MTDotNetCore.WinFormsAppSqlnjection
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(79, 58);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(240, 27);
+            txtEmail.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(79, 105);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(240, 27);
+            txtPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(79, 150);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 34);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "&Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(420, 326);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
---
+++ MTDotNetCore.WinFormsAppSqlnjection/FrmLogin.cs
@@ -0,0 +1,15 @@
+namespace MTDotNetCore.WinFormsAppSqlnjection
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
