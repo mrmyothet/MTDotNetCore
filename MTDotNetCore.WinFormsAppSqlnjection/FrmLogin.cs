bfeb7125986aa8aea7c26d164c513200c170c66f
Myo Thet <mr.myothet@gmail.com>
2024-06-10 18:30:08 +0700

    Windows Form - Sql Injection - Works without using parameters

--- MTDotNetCore.WinFormsAppSqlnjection/FrmLogin.cs
+++ MTDotNetCore.WinFormsAppSqlnjection/FrmLogin.cs
@@ -1,15 +1,39 @@
+using MTDotNetCore.Shared;
+
 namespace MTDotNetCore.WinFormsAppSqlnjection
 {
     public partial class frmLogin : Form
     {
+        private readonly DapperService _dapperService;
         public frmLogin()
         {
             InitializeComponent();
+            _dapperService = new DapperService(ConnectionStrings.builder.ConnectionString);
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            // 123' or 1=1 + '
 
+            string query = $"select * from tbl_user where email = '{email}' and password='{password}'";
+            var model = _dapperService.QueryFirstOrDefault<UserModel>(query);
+
+            if (model is null)
+            {
+                MessageBox.Show("The user doesn't exist.");
+                return;
+            }
+
+            MessageBox.Show("Is Admin : " + model.Email);
         }
+    }
+
+    public class UserModel
+    {
+        public string Email { get; set; }
+
+        public bool IsAdmin { get; set; }
     }
 }
