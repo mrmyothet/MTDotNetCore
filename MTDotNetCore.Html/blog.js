d7cc7193ed8902b2be85297efaef1e85e0cd3c0d
Myo Thet <mr.myothet@gmail.com>
2024-06-17 11:22:55 +0700

    SweetAlert - Use sweetAlert for successMessage and errorMessage in Blog CRUD

--- MTDotNetCore.Html/blog.js
+++ MTDotNetCore.Html/blog.js
@@ -109,11 +109,19 @@ $('#btnSave').click(function () {
 });
 
 function successMessage(message) {
-    alert(message);
+    Swal.fire({
+        title: "Success!",
+        text: message,
+        icon: "success"
+    });
 }
 
 function errorMessage(message) {
-    alert(message);
+    Swal.fire({
+        title: "Error!",
+        text: message,
+        icon: "error"
+    });
 }
 
 function clearControls() {
