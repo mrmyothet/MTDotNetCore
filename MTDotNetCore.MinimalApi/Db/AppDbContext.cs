54461f5161cff04eac653a498024146dca50c4d2
Myo Thet <mr.myothet@gmail.com>
2024-06-20 23:40:43 +0700

    Minimal API - Delete Weather Forecast code and Add some references

---
+++ MTDotNetCore.MinimalApi/Db/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MTDotNetCore.MinimalApi.Models;
+
+namespace MTDotNetCore.MinimalApi.Db
+{
+    public class AppDbContext : DbContext
+    {
+        // Replace with Dependency Injection configured at Program.cs
+
+        public AppDbContext(DbContextOptions options)
+            : base(options) { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //base.OnConfiguring(optionsBuilder);
+
+        //    optionsBuilder.UseSqlServer(
+        //        ConnectionStrings.sqlConnectionStringBuilder.ConnectionString
+        //    );
+        //}
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
