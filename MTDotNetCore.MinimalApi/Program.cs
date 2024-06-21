d01853815645e6c56ca9678b501dab553b405050
Myo Thet <mr.myothet@gmail.com>
2024-06-21 16:18:01 +0700

    Minimal API - Blogs - CRUD - Create, Read, Update, Delete

--- MTDotNetCore.MinimalApi/Program.cs
+++ MTDotNetCore.MinimalApi/Program.cs
@@ -1,6 +1,7 @@
 using System.Collections.Immutable;
 using Microsoft.EntityFrameworkCore;
 using MTDotNetCore.MinimalApi.Db;
+using MTDotNetCore.MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,12 +31,79 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World");
 
+// Get List
 app.MapGet(
     "api/Blog",
     async (AppDbContext db) =>
     {
         var lst = await db.Blogs.AsNoTracking().ToListAsync();
         return Results.Ok(lst);
+    }
+);
+
+app.MapGet(
+    "api/Blog/{id}",
+    (AppDbContext db, int id) =>
+    {
+        var item = db.Blogs.FirstOrDefault(b => b.BlogId == id);
+        if (item is null)
+        {
+            return Results.NotFound("No data found for Id: " + id);
+        }
+
+        return Results.Ok(item);
+    }
+);
+
+// Create
+app.MapPost(
+    "api/Blog",
+    async (AppDbContext db, BlogModel newBlog) =>
+    {
+        db.Blogs.Add(newBlog);
+        int result = await db.SaveChangesAsync();
+
+        string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+        return Results.Ok(message);
+    }
+);
+
+// Update
+app.MapPut(
+    "api/Blog/{id}",
+    async (AppDbContext db, int id, BlogModel editBlog) =>
+    {
+        var item = db.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            return Results.NotFound("No data found for Id: " + id);
+        }
+
+        item.BlogTitle = editBlog.BlogTitle;
+        item.BlogAuthor = editBlog.BlogAuthor;
+        item.BlogContent = editBlog.BlogContent;
+        var result = await db.SaveChangesAsync();
+
+        string message = result > 0 ? "Update Successful." : "Update Failed";
+        return Results.Ok(message);
+    }
+);
+
+app.MapDelete(
+    "api/Blog/{id}",
+    async (AppDbContext db, int id) =>
+    {
+        var item = db.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            return Results.NotFound("No data found for Id: " + id);
+        }
+
+        db.Blogs.Remove(item);
+        int result = await db.SaveChangesAsync();
+
+        string message = result > 0 ? "Delete Successful." : "Delete Failed.";
+        return Results.Ok(message);
     }
 );
 
