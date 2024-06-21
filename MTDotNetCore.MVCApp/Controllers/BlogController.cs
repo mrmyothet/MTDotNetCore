cf50c15d9cb0570dffc471b8a9b55b34254639d7
Myo Thet <mr.myothet@gmail.com>
2024-06-21 22:50:49 +0700

    MVC - Blog Create

--- MTDotNetCore.MVCApp/Controllers/BlogController.cs
+++ MTDotNetCore.MVCApp/Controllers/BlogController.cs
@@ -16,8 +16,28 @@ namespace MTDotNetCore.MVCApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            List<BlogModel> lst = await _db.Blogs.ToListAsync();
+            List<BlogModel> lst = await _db.Blogs.OrderByDescending(x => x.BlogId).ToListAsync();
             return View(lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> Save(BlogModel blog)
+        {
+            await _db.Blogs.AddAsync(blog);
+            var result = await _db.SaveChangesAsync();
+            if (result > 0)
+            {
+                return Redirect("/Blog");
+            }
+
+            return View("BlogCreate");
         }
     }
 }
