9e67a57e398ade004854dee5c7eb53ba76e9d780
Myo Thet <mr.myothet@gmail.com>
2024-07-03 18:06:52 +0700

    Apex Chart - Simple Pie Chart

---
+++ MTDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.MvcChartApp.Models;
+
+namespace MTDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult SimplePieChart()
+        {
+            PieChartModel model = new PieChartModel();
+            model.Series = new List<int> { 44, 55, 13, 43, 22 };
+            model.Labels = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+
+            return View(model);
+        }
+    }
+}
---
+++ MTDotNetCore.MvcChartApp/Models/PieChartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTDotNetCore.MvcChartApp.Models
+{
+    public class PieChartModel
+    {
+        public List<int> Series { get; set; }
+
+        public List<string> Labels { get; set; }
+    }
+}
