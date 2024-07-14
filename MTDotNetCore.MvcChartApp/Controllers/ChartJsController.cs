907a42f04c3d618dd917a9ea6a2171b3aed2ccd9
Myo Thet <mr.myothet@gmail.com>
2024-07-14 13:01:51 +0700

    ChartJs - Example Bar Chart

---
+++ MTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.MvcChartApp.Models.ChartJs;
+
+namespace MTDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExampleBarChart()
+        {
+            ExampleBarChartModel model = new ExampleBarChartModel();
+            model.Title = "No of Votes";
+            model.LabelList = new List<string>
+            {
+                "Red",
+                "Blue",
+                "Yellow",
+                "Green",
+                "Purple",
+                "Orange"
+            };
+
+            model.Data = new List<int> { 12, 19, 3, 5, 2, 3 };
+
+            return View(model);
+        }
+    }
+}
---
+++ MTDotNetCore.MvcChartApp/Models/ChartJs/ExampleBarChartModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MTDotNetCore.MvcChartApp.Models.ChartJs;
+
+public class ExampleBarChartModel
+{
+    public string? Title { get; set; }
+
+    public List<string>? LabelList { get; set; }
+
+    public List<int>? Data { get; set; }
+}
