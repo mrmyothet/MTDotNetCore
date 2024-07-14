ab8b702eea96ceaaff7ca1eadbb7d9c7e4f34666
Myo Thet <mr.myothet@gmail.com>
2024-07-14 14:44:05 +0700

    HighCharts - Demo - Pie Chart and Bootstrap NavBar Dropdown Menu

---
+++ MTDotNetCore.MvcChartApp/Controllers/HighChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
