ee7ebfe2c9077eb401923f292f8316017c14dc19
Myo Thet <mr.myothet@gmail.com>
2024-07-14 13:40:49 +0700

    ChartJs - Interpolation Line Chart

--- MTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
+++ MTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -1,27 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTDotNetCore.MvcChartApp.Models.ChartJs;
 
-namespace MTDotNetCore.MvcChartApp.Controllers
+namespace MTDotNetCore.MvcChartApp.Controllers;
+
+public class ChartJsController : Controller
 {
-    public class ChartJsController : Controller
+    public IActionResult ExampleBarChart()
     {
-        public IActionResult ExampleBarChart()
-        {
-            ExampleBarChartModel model = new ExampleBarChartModel();
-            model.Title = "No of Votes";
-            model.LabelList = new List<string>
-            {
-                "Red",
-                "Blue",
-                "Yellow",
-                "Green",
-                "Purple",
-                "Orange"
-            };
+        ExampleBarChartModel model = new ExampleBarChartModel();
+        model.Title = "No of Votes";
+        model.LabelList = new List<string> { "Red", "Blue", "Yellow", "Green", "Purple", "Orange" };
 
-            model.Data = new List<int> { 12, 19, 3, 5, 2, 3 };
+        model.Data = new List<int> { 12, 19, 3, 5, 2, 3 };
 
-            return View(model);
-        }
+        return View(model);
+    }
+
+    public IActionResult BorderRadiusBarChart()
+    {
+        return View();
+    }
+
+    public IActionResult InterpolationLineChart()
+    {
+        return View();
     }
 }
