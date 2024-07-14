bd374abee46e5e105c094eea3e24978d2f067e3b
Myo Thet <mr.myothet@gmail.com>
2024-07-14 17:19:52 +0700

    canvasJS - Line Chart

---
+++ MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
