ccff80d4842eff45b31b6f734e0d6892b7959f77
Myo Thet <mr.myothet@gmail.com>
2024-07-15 17:01:44 +0700

    HighCharts - Pie Chart - Send data from Controller to View

--- MTDotNetCore.MvcChartApp/Controllers/HighChartController.cs
+++ MTDotNetCore.MvcChartApp/Controllers/HighChartController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.MvcChartApp.Models.HighChart;
 
-namespace MTDotNetCore.MvcChartApp.Controllers
+namespace MTDotNetCore.MvcChartApp.Controllers;
+
+public class HighChartController : Controller
 {
-    public class HighChartController : Controller
+    public IActionResult PieChart()
     {
-        public IActionResult PieChart()
+        PieChartModel model = new PieChartModel();
+        model.DataList = new List<DataPoint>()
         {
-            return View();
-        }
+            new DataPoint() { Name = "Water", y = 55.02 },
+            new DataPoint() { Name = "Fat", y = 26.71 },
+            new DataPoint() { Name = "Carbohydrates", y = 1.09 },
+            new DataPoint() { Name = "Protein", y = 15.5 },
+            new DataPoint() { Name = "Ash", y = 1.68 },
+        };
+
+        return View(model);
     }
 }
---
+++ MTDotNetCore.MvcChartApp/Models/HighChart/PieChartModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MTDotNetCore.MvcChartApp.Models.HighChart
+{
+    public class PieChartModel
+    {
+        public List<DataPoint>? DataList { get; set; }
+    }
+
+    public class DataPoint
+    {
+        public string? Name { get; set; }
+        public double y { get; set; }
+    }
+}
