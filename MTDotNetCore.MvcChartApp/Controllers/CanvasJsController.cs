54106b915e12da94e18ade89e290481f060790f6
Myo Thet <mr.myothet@gmail.com>
2024-07-15 11:36:00 +0700

    Serilog - MVC Web App

--- MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
+++ MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -4,8 +4,16 @@ namespace MTDotNetCore.MvcChartApp.Controllers
 {
     public class CanvasJsController : Controller
     {
+        private readonly ILogger<CanvasJsController> _logger;
+
+        public CanvasJsController(ILogger<CanvasJsController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult LineChart()
         {
+            _logger.LogInformation("CanvasJS - Line Chart.");
             return View();
         }
     }
