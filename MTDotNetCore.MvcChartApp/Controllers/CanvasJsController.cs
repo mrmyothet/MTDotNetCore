3e770edb134ce3e727f389e4f929b34fb468456c
Myo Thet <mr.myothet@gmail.com>
2024-07-20 16:42:41 +0700

    CanvasJS - Line Chart - Send data from Controller to View

--- MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
+++ MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.MvcChartApp.Models.CanvasJs;
 
 namespace MTDotNetCore.MvcChartApp.Controllers
 {
@@ -14,7 +15,37 @@ namespace MTDotNetCore.MvcChartApp.Controllers
         public IActionResult LineChart()
         {
             _logger.LogInformation("CanvasJS - Line Chart.");
-            return View();
+
+            LineChartModel model = new LineChartModel();
+            model.DataList = new List<DataPoint>
+            {
+                new DataPoint() { y = 450 },
+                new DataPoint() { y = 414 },
+                new DataPoint()
+                {
+                    y = 520,
+                    indexLabel = "\u2191 highest",
+                    markerColor = "red",
+                    markerType = "triangle"
+                },
+                new DataPoint() { y = 460 },
+                new DataPoint() { y = 450 },
+                new DataPoint() { y = 500 },
+                new DataPoint() { y = 480 },
+                new DataPoint() { y = 480 },
+                new DataPoint()
+                {
+                    y = 410,
+                    indexLabel = "\u2193 lowest",
+                    markerColor = "DarkSlateGrey",
+                    markerType = "cross"
+                },
+                new DataPoint() { y = 500 },
+                new DataPoint() { y = 480 },
+                new DataPoint() { y = 510 }
+            };
+
+            return View(model);
         }
     }
 }
---
+++ MTDotNetCore.MvcChartApp/Models/CanvasJs/LineChartModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MTDotNetCore.MvcChartApp.Models.CanvasJs;
+
+public class LineChartModel
+{
+    public List<DataPoint> DataList { get; set; } = new List<DataPoint>();
+}
+
+public class DataPoint
+{
+    public int y { get; set; }
+    public string? indexLabel { get; set; }
+    public string? markerColor { get; set; }
+    public string? markerType { get; set; }
+}
