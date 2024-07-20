2ad1d707a86965267081f4c141710beb7f6f3487
Myo Thet <mr.myothet@gmail.com>
2024-07-20 17:12:21 +0700

    CanvasJS - Multi Series Area Chart

--- MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
+++ MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -1,51 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTDotNetCore.MvcChartApp.Models.CanvasJs;
 
-namespace MTDotNetCore.MvcChartApp.Controllers
+namespace MTDotNetCore.MvcChartApp.Controllers;
+
+public class CanvasJsController : Controller
 {
-    public class CanvasJsController : Controller
+    private readonly ILogger<CanvasJsController> _logger;
+
+    public CanvasJsController(ILogger<CanvasJsController> logger)
     {
-        private readonly ILogger<CanvasJsController> _logger;
+        _logger = logger;
+    }
 
-        public CanvasJsController(ILogger<CanvasJsController> logger)
+    public IActionResult LineChart()
+    {
+        _logger.LogInformation("CanvasJS - Line Chart.");
+
+        LineChartModel model = new LineChartModel();
+        model.DataList = new List<DataPoint>
         {
-            _logger = logger;
-        }
-
-        public IActionResult LineChart()
-        {
-            _logger.LogInformation("CanvasJS - Line Chart.");
-
-            LineChartModel model = new LineChartModel();
-            model.DataList = new List<DataPoint>
+            new DataPoint() { y = 450 },
+            new DataPoint() { y = 414 },
+            new DataPoint()
             {
-                new DataPoint() { y = 450 },
-                new DataPoint() { y = 414 },
-                new DataPoint()
-                {
-                    y = 520,
-                    indexLabel = "\u2191 highest",
-                    markerColor = "red",
-                    markerType = "triangle"
-                },
-                new DataPoint() { y = 460 },
-                new DataPoint() { y = 450 },
-                new DataPoint() { y = 500 },
-                new DataPoint() { y = 480 },
-                new DataPoint() { y = 480 },
-                new DataPoint()
-                {
-                    y = 410,
-                    indexLabel = "\u2193 lowest",
-                    markerColor = "DarkSlateGrey",
-                    markerType = "cross"
-                },
-                new DataPoint() { y = 500 },
-                new DataPoint() { y = 480 },
-                new DataPoint() { y = 510 }
-            };
+                y = 520,
+                indexLabel = "\u2191 highest",
+                markerColor = "red",
+                markerType = "triangle"
+            },
+            new DataPoint() { y = 460 },
+            new DataPoint() { y = 450 },
+            new DataPoint() { y = 500 },
+            new DataPoint() { y = 480 },
+            new DataPoint() { y = 480 },
+            new DataPoint()
+            {
+                y = 410,
+                indexLabel = "\u2193 lowest",
+                markerColor = "DarkSlateGrey",
+                markerType = "cross"
+            },
+            new DataPoint() { y = 500 },
+            new DataPoint() { y = 480 },
+            new DataPoint() { y = 510 }
+        };
 
-            return View(model);
-        }
+        return View(model);
+    }
+
+    public IActionResult MultiSeriesAreaChart()
+    {
+        return View();
     }
 }
