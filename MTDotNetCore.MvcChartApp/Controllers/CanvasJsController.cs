cc937542755255cbf6f87e408bd1f7c309b87b38
Myo Thet <mr.myothet@gmail.com>
2024-07-21 01:33:16 +0700

    [In Progress] - CanvasJS - MultiSerieAreaChart

--- MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
+++ MTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using MTDotNetCore.MvcChartApp.Models.CanvasJs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MTDotNetCore.MvcChartApp.Controllers;
 
@@ -50,6 +55,68 @@ public class CanvasJsController : Controller
 
     public IActionResult MultiSeriesAreaChart()
     {
+        MultiSeriesAreaChartModel model = new MultiSeriesAreaChartModel();
+        model.SerieAreaList = new List<SerieArea>();
+
+        SerieArea Received = new SerieArea()
+        {
+            name = "Received",
+            showInLegend = true,
+            legendMarkerType = "square",
+            type = "area",
+            color = "rgba(40,175,101,0.6)",
+            markerSize = 0,
+            dataPoints = new List<Datapoint>()
+            {
+                new Datapoint() { x = new DateTime(2017, 1, 16), y = 220 },
+                new Datapoint() { x = new DateTime(2017, 1, 7), y = 120 },
+                new Datapoint() { x = new DateTime(2017, 1, 8), y = 144 },
+                new Datapoint() { x = new DateTime(2017, 1, 9), y = 162 },
+                new Datapoint() { x = new DateTime(2017, 1, 10), y = 129 },
+                new Datapoint() { x = new DateTime(2017, 1, 11), y = 109 },
+                new Datapoint() { x = new DateTime(2017, 1, 12), y = 129 }
+            }
+        };
+
+        SerieArea Sent = new SerieArea()
+        {
+            name = "Sent",
+            showInLegend = true,
+            legendMarkerType = "square",
+            type = "area",
+            color = "rgba(0,75,141,0.7)",
+            markerSize = 0,
+            dataPoints = new List<Datapoint>()
+            {
+                new Datapoint() { x = new DateTime(2017, 1, 6), y = 42 },
+                new Datapoint() { x = new DateTime(2017, 1, 7), y = 34 },
+                new Datapoint() { x = new DateTime(2017, 1, 8), y = 29 },
+                new Datapoint() { x = new DateTime(2017, 1, 9), y = 42 },
+                new Datapoint() { x = new DateTime(2017, 1, 10), y = 53 },
+                new Datapoint() { x = new DateTime(2017, 1, 11), y = 15 },
+                new Datapoint() { x = new DateTime(2017, 1, 12), y = 12 }
+            }
+        };
+
+        model.SerieAreaList.Add(Received);
+        model.SerieAreaList.Add(Sent);
+
+        var settings = new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter> { new CustomDateTimeConverter() }
+        };
+
+        var receivedJson = JsonConvert.SerializeObject(Received.dataPoints, settings);
+        ViewBag.receivedJson = receivedJson;
+
         return View();
+    }
+}
+
+public class CustomDateTimeConverter : IsoDateTimeConverter
+{
+    public CustomDateTimeConverter()
+    {
+        DateTimeFormat = "yyyy, M, dd"; // Adjust month to be zero-based
     }
 }
---
+++ MTDotNetCore.MvcChartApp/Models/CanvasJs/MultiSeriesAreaChartModel.cs
@@ -0,0 +1,26 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MTDotNetCore.MvcChartApp.Models.CanvasJs
+{
+    public class MultiSeriesAreaChartModel
+    {
+        public List<SerieArea> SerieAreaList { get; set; } = new List<SerieArea>();
+    }
+
+    public class SerieArea
+    {
+        public string name { get; set; }
+        public bool showInLegend { get; set; }
+        public string legendMarkerType { get; set; }
+        public string type { get; set; }
+        public string color { get; set; }
+        public int markerSize { get; set; }
+        public List<Datapoint>? dataPoints { get; set; }
+    }
+
+    public class Datapoint
+    {
+        public DateTime x { get; set; }
+        public int y { get; set; }
+    }
+}
