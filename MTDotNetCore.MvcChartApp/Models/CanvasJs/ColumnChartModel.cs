2f3f6018431fcc3898eee4d50a10a3a991bd923b
Myo Thet <mr.myothet@gmail.com>
2024-07-21 17:09:55 +0700

    canvasJS - Column Chart - Bring Model data from Controller to View

---
+++ MTDotNetCore.MvcChartApp/Models/CanvasJs/ColumnChartModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MTDotNetCore.MvcChartApp.Models.CanvasJs
+{
+    public class ColumnChartModel
+    {
+        public List<ColumnChartDatapoint>? Reserves { get; set; }
+        public List<ColumnChartDatapoint>? Production { get; set; }
+    }
+
+    public class ColumnChartDatapoint
+    {
+        public string? label { get; set; }
+        public float y { get; set; }
+    }
+}
