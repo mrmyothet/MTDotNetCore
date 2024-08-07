04db4808a3ca67188b85510517212a908340636b
Myo Thet <mr.myothet@gmail.com>
2024-08-07 23:32:04 +0700

    Pagination - Add MVC Project and configure EFCore AppDbContext

---
+++ MTDotNetCore.PaginationMvc/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.PaginationMvc.DB;
+
+namespace MTDotNetCore.PaginationMvc.Controllers;
+
+public class BlogController : Controller
+{
+    private readonly AppDbContext _dbContext;
+
+    public BlogController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IActionResult Index()
+    {
+        var lst = _dbContext.Blogs.ToList();
+        ViewBag.Count = lst.Count();
+        return View();
+    }
+}
---
+++ MTDotNetCore.PaginationMvc/DB/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTDotNetCore.PaginationMvc.Models;
+
+namespace MTDotNetCore.PaginationMvc.DB;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
