65e37c2d6fc05ec2e3d3597104c4b8e8d0e29250
Myo Thet <mr.myothet@gmail.com>
2024-08-07 10:16:48 +0700

    Pagination - Generate Blog records by calling API from Console App

--- MTDotNetCore.PaginationConsoleApp/Program.cs
+++ MTDotNetCore.PaginationConsoleApp/Program.cs
@@ -1,5 +1,24 @@
-﻿HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("https://localhost:7254") };
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("https://localhost:7254") };
 string blogEndPoint = "/api/blog";
+
+int NoOfRecords = 395;
+for (int i = 0; i < NoOfRecords; i++)
+{
+    int rowNo = i + 1;
+    BlogModel newBlog = new BlogModel()
+    {
+        blogTitle = "Sample Title " + rowNo,
+        blogAuthor = "Sample Author " + rowNo,
+        blogContent = "Sample Content " + rowNo
+    };
+
+    await CreateBlogPostAsync(_httpClient, newBlog);
+}
 
 var response = await _httpClient.GetAsync(blogEndPoint);
 
@@ -8,14 +27,31 @@ if (!response.IsSuccessStatusCode)
 
 Console.WriteLine("Success Status Code");
 string jsonString = await response.Content.ReadAsStringAsync();
-Console.WriteLine(jsonString);
+
+if (!String.IsNullOrEmpty(jsonString))
+{
+    List<BlogModel> lst = JsonConvert.DeserializeObject<List<BlogModel>>(jsonString)!;
+    Console.WriteLine(lst.Count);
+}
 
 Console.ReadLine();
+
+async Task CreateBlogPostAsync(HttpClient httpClient, BlogModel newblog)
+{
+    string strNewBlog = JsonConvert.SerializeObject(newblog);
+    HttpContent content = new StringContent(strNewBlog, Encoding.UTF8, Application.Json);
+    var result = await httpClient.PostAsync(blogEndPoint, content);
+    if (result.IsSuccessStatusCode)
+    {
+        string strResponse = await result.Content.ReadAsStringAsync();
+        Console.WriteLine(strResponse);
+    }
+}
 
 public class BlogModel
 {
     public int blogId { get; set; }
-    public string blogTitle { get; set; }
-    public string blogAuthor { get; set; }
-    public string blogContent { get; set; }
+    public string? blogTitle { get; set; }
+    public string? blogAuthor { get; set; }
+    public string? blogContent { get; set; }
 }
