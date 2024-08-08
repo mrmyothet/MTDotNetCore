6937a40f9ca146649d00d8f2505e7ec69b2cb16d
Myo Thet <mr.myothet@gmail.com>
2024-08-08 13:46:02 +0700

    Pagination - records less than or equal 90

--- MTDotNetCore.PaginationMvc/Controllers/BlogController.cs
+++ MTDotNetCore.PaginationMvc/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTDotNetCore.PaginationMvc.DB;
+using MTDotNetCore.PaginationMvc.Models;
 
 namespace MTDotNetCore.PaginationMvc.Controllers;
 
@@ -12,10 +13,36 @@ public class BlogController : Controller
         _dbContext = dbContext;
     }
 
-    public IActionResult Index()
+    [ActionName("Index")]
+    public IActionResult Index(int pageNo = 1, int pageSize = 10)
     {
-        var lst = _dbContext.Blogs.ToList();
-        ViewBag.Count = lst.Count();
-        return View();
+        int rowCount = _dbContext.Blogs.Count();
+
+        int pageCount = rowCount / pageSize;
+        if (rowCount % pageSize > 0)
+            pageCount++;
+
+        //
+        if (pageNo > pageCount)
+        {
+            return View();
+        }
+
+        int skippedRows = (pageNo - 1) * pageSize;
+
+        BlogResponseModel model = new BlogResponseModel()
+        {
+            Data = _dbContext
+                .Blogs
+                //.OrderByDescending(x => x.BlogId)
+                .Skip(skippedRows)
+                .Take(pageSize)
+                .ToList(),
+            PageNo = pageNo,
+            PageSize = pageSize,
+            PageCount = pageCount
+        };
+
+        return View(model);
     }
 }
