4b8d344553763e8728e13efdc2260cfdaae7ef2c
Myo Thet <mr.myothet@gmail.com>
2024-08-09 13:21:14 +0700

    Realtime Chart - PieChart/Create

--- MTDotNetCore.RealtimeChartApp/Controllers/PieChartController.cs
+++ MTDotNetCore.RealtimeChartApp/Controllers/PieChartController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.RealtimeChartApp.Models;
 
 namespace MTDotNetCore.RealtimeChartApp.Controllers
 {
     public class PieChartController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public PieChartController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +20,13 @@ namespace MTDotNetCore.RealtimeChartApp.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Save(TblPieChart reqModel)
+        {
+            await _db.TblPieCharts.AddAsync(reqModel);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Create");
         }
     }
 }
