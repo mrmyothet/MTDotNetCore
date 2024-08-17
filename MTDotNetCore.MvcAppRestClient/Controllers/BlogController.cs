f1725a4d0425f578e4ba61f740cc676eeb32dc68
Myo Thet <mr.myothet@gmail.com>
2024-08-17 19:35:36 +0700

    Consume API - RestClient - Edit, Update, Delete

--- MTDotNetCore.MvcAppRestClient/Controllers/BlogController.cs
+++ MTDotNetCore.MvcAppRestClient/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTDotNetCore.MvcAppRestClient.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using RestSharp;
 
 namespace MTDotNetCore.MvcAppRestClient.Controllers
@@ -16,9 +17,7 @@ namespace MTDotNetCore.MvcAppRestClient.Controllers
 
         public async Task<IActionResult> Index(int pageNo = 1, int pageSize = 10)
         {
-            string resource = $"/api/blog/{pageNo}/{pageSize}";
-
-            RestRequest restRequest = new RestRequest(resource, Method.Get);
+            RestRequest restRequest = new RestRequest($"/api/blog/{pageNo}/{pageSize}", Method.Get);
             var response = await _restClient.ExecuteAsync(restRequest);
             if (!response.IsSuccessStatusCode)
                 return RedirectToAction("Error");
@@ -27,6 +26,61 @@ namespace MTDotNetCore.MvcAppRestClient.Controllers
             BlogResponseModel model = JsonConvert.DeserializeObject<BlogResponseModel>(jsonString)!;
 
             return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> Save(BlogModel model)
+        {
+            string jsonString = JsonConvert.SerializeObject(model);
+
+            RestRequest restRequest = new RestRequest("/api/blog", Method.Post);
+            restRequest.AddBody(jsonString);
+            await _restClient.ExecuteAsync(restRequest);
+
+            return Redirect("/Blog");
+        }
+
+        [ActionName("Edit")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            RestRequest restRquest = new RestRequest($"/api/blog/{id}", Method.Get);
+            var response = await _restClient.ExecuteAsync(restRquest);
+            if (!response.IsSuccessStatusCode)
+                return Redirect("Error");
+
+            string jsonString = response.Content!;
+            BlogModel model = JsonConvert.DeserializeObject<BlogModel>(jsonString)!;
+
+            return View(model);
+        }
+
+        [ActionName("Update")]
+        public async Task<IActionResult> Update(int id, BlogModel model)
+        {
+            RestRequest restRequest = new RestRequest($"/api/blog/{id}", Method.Put);
+            restRequest.AddBody(model);
+            var response = await _restClient.ExecuteAsync(restRequest);
+            if (!response.IsSuccessStatusCode)
+                return Redirect("Error");
+
+            return Redirect("/blog");
+        }
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            RestRequest restRequest = new RestRequest($"/api/blog/{id}", Method.Delete);
+            var response = await _restClient.ExecuteAsync(restRequest);
+            if (!response.IsSuccessStatusCode)
+                return Redirect("Error");
+
+            return Redirect("/blog");
         }
     }
 }
