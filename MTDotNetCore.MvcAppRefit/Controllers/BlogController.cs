9f14f40889c88b2712ffa3d4ecc872a6bc7dfe31
Myo Thet <mr.myothet@gmail.com>
2024-08-19 22:27:49 +0700

    Consume API - Refit - Create, Update, Delete

--- MTDotNetCore.MvcAppRefit/Controllers/BlogController.cs
+++ MTDotNetCore.MvcAppRefit/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MTDotNetCore.MvcAppRefit.Models;
 
 namespace MTDotNetCore.MvcAppRefit.Controllers
 {
@@ -17,6 +18,44 @@ namespace MTDotNetCore.MvcAppRefit.Controllers
             var model = await _blogApi.GetBlogs(pageNo, PageSize);
 
             return View(model);
+        }
+
+        [ActionName("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [ActionName("Save")]
+        public async Task<IActionResult> Save(BlogModel model)
+        {
+            var response = await _blogApi.CreateBlog(model);
+
+            return Redirect("/blog");
+        }
+
+        [ActionName("Edit")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _blogApi.GetBlog(id);
+
+            return View(model);
+        }
+
+        [ActionName("Update")]
+        public async Task<IActionResult> Update(int id, BlogModel model)
+        {
+            var response = await _blogApi.UpdateBlog(id, model);
+
+            return Redirect("/blog");
+        }
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _blogApi.DeleteBlog(id);
+
+            return Redirect("/blog");
         }
     }
 }
