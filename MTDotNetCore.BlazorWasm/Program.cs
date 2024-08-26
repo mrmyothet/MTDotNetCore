19d095261ef1e655739399a511ea0cd36410f4ad
Myo Thet <mr.myothet@gmail.com>
2024-08-26 22:56:51 +0700

    Blazor Wasm - HttpClient Call to RestApi - api/blog

--- MTDotNetCore.BlazorWasm/Program.cs
+++ MTDotNetCore.BlazorWasm/Program.cs
@@ -6,6 +6,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string apiUrl = "https://localhost:7233";
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    //BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    BaseAddress = new Uri(apiUrl)
+});
 
 await builder.Build().RunAsync();
--- MTDotNetCore.RestApi/Program.cs
+++ MTDotNetCore.RestApi/Program.cs
@@ -2,9 +2,22 @@ using Microsoft.EntityFrameworkCore;
 using MTDotNetCore.RestApi.Db;
 using MTDotNetCore.Shared;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:7211", "http://localhost:5226");
+        }
+    );
+});
 
 builder.Services.AddControllers();
 
@@ -39,6 +52,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
